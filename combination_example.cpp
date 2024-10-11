6a787fa000644930fbd23484f6e1eda5f151fe69
Alexandra Stankovich <stankovichalexandra@gmail.com>
2024-10-10 23:26:19 -0700

    C++ implementation of combination calculation

--- combination_example.cpp
+++ combination_example.cpp
@@ -3,25 +3,37 @@
 using namespace std;
 
 /*
- * *** STUDENTS SHOULD WRITE CODE FOR THIS FUNCTION ***
+ * Calculate factorial
  */
-uint16_t factorial(const uint16_t x) { return x; }
+uint16_t factorial(const uint16_t x)
+{
+    if (x <= 1)
+        return 1;
+    else
+        return x * factorial(x - 1);
+}
 
 /*
- * *** STUDENTS SHOULD WRITE CODE FOR THIS FUNCTION ***
+ * main function
  */
 int main() {
-    int n;
+    int n, k;
 
     // get and validate user input
     cout << "Enter n: ";
     cin >> n;
+    cout << "Enter k: ";
+    cin >> k;
 
-    // calculate C(n,k) = n! / (k! * (n-k)!)
-    uint16_t c_n_k = factorial(n);
+    if ((k <= 0) || (n <= 0))
+        // write out the placeholder error value
+        cout << "result = " << -1 << endl;
+    else {
+        // calculate C(n,k) = n! / (k! * (n-k)!)
+        uint16_t c_n_k = factorial(n) / (factorial(k) * factorial(n - k));
 
-    // write out results
-    cout << "result = " << c_n_k << endl;
-
+        // write out results
+        cout << "result = " << c_n_k << endl;
+    }
     return 0;
 }
